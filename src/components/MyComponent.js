fede01c0df6f9673d1656f2803a2412747d5e85b
Shannon <shannon@Shannons-MacBook-Pro.local>
2023-05-15 22:43:02 -0500

    Add React-only setup

---
+++ src/components/MyComponent.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+function MyComponent(props) {
+    return (
+        <div>
+            <h1>{props.title}</h1>
+            <h1>{props.message}</h1>
+        </div>
+    )
+}
+
+export default MyComponent;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,20 @@
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import MyApp from './components/MyApp';
+
+import './styles/style.css';
 import './styles/reset.css';
-import './styles/layout.css';
-import './styles/typo.css';
-import './styles/shaping.css';
-import './styles/colors.css';
 import iconsArray from './scripts/icons.js';
+
+import storage from './scripts/storage.js';
+import events from './scripts/events.js';
 
 // import Img from './img.png';
 // import XmlData from './data.xml';
 // import CsvData from './data.csv';
 
 // manager function here
-
 console.log('webpack is working!');
+const domNode = document.getElementById('root');
+const root = createRoot(domNode);
+root.render(<MyApp />);
